55cbfeadf71cbb3b5e653cdc46111e1866e62840
TC <2216685752@qq.com>
2020-10-21 00:19:13 +0800

    Chapter 12

---
+++ Pygame/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """存储游戏的设置类"""
+
+    def __init__(self):
+        """初始化游戏设置"""
+        #屏幕设置
+        self.screen_width = 1000
+        self.screen_height = 700
+        self.bg_color = (230,230,230)
