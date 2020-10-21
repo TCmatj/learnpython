2433cba34d2b7d482605a3d1fcff98b3ac555b31
TC <2216685752@qq.com>
2020-10-21 15:13:23 +0800

    Chapter 12

--- Pygame/ship.py
+++ Pygame/ship.py
@@ -18,10 +18,13 @@ class Ship():
 
         #飞船属性center中存储小数
         self.center = float(self.rect.centerx)
+        self.centery = float(self.rect.bottom)
         
         #移动标志
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
         
     def update(self):
         """根据移动标志调整飞船位置"""
@@ -30,7 +33,12 @@ class Ship():
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > self.screen_rect.left:
             self.center -= self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.centery -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.centery += self.ai_settings.ship_speed_factor
         self.rect.centerx = self.center
+        self.rect.bottom = self.centery
             
     def blitme(self):
         """在指定位置绘制飞船"""
